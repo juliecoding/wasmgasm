28e7fcdcd7baf39fd333e6b7b7adf778cea8726a
juliecoding <julie.akohler@gmail.com>
2017-08-17 20:56:44 -0600

    I can't believe it actually compiled.

--- random.cc
+++ random.cc
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int main() {;
-  srand((unsigned)time(0))
+int main() {
+  srand((unsigned)time(0));
   int random_integer;
-  random_integer = (rand()%10)+1;
+  random_integer = (rand()%256)+1;
   cout << random_integer << endl;
 }
